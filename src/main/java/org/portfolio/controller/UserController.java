95401a4c8dfad003df96fda8209cd341effc9644
Boris Velkov <b.velkov@abv.bg>
2026-06-08 07:05:01 +0300

    Register get and post methods added.

--- src/main/java/org/portfolio/controller/UserController.java
+++ src/main/java/org/portfolio/controller/UserController.java
@@ -1,7 +1,11 @@
 package org.portfolio.controller;
 
+import jakarta.validation.Valid;
+import org.portfolio.models.dto.user.UserRegisterDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -9,5 +13,21 @@ public class UserController {
     @GetMapping("/")
     public String getHomePage(){
         return "index";
+    }
+
+    @GetMapping("/reg")
+    public String getRegister(){
+
+        return "register";
+    }
+
+    @PostMapping("/reg")
+    public String postRegister(@Valid UserRegisterDTO userRegisterDTO, Model model){
+
+        if (!model.containsAttribute("userRegisterDTO")){
+            model.addAttribute("userRegisterDTO", UserRegisterDTO.class);
+        }
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/org/portfolio/models/dto/user/UserRegisterDTO.java
@@ -0,0 +1,10 @@
+package org.portfolio.models.dto.user;
+
+public class UserRegisterDTO {
+
+    private String username;
+    private String password;
+    private String email;
+    private String confirmPassword;
+
+}
